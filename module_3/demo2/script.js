bd775019ecc78752e2aa2c387d80a344ae7ed6ab
stropee <stropee@campus-eni.fr>
2025-06-03 16:02:36 +0200

    module 3 demos

---
+++ module_3/demo2/script.js
@@ -0,0 +1,18 @@
+let joke;
+fetch("https://api.chucknorris.io/jokes/random",
+    /*{method : "GET", body : JSON.stringify({name: "Michel"})}*/)
+    .then(response => response.text())
+    .then(data => {
+        data = JSON.parse(data) //JSON.stringify()
+        joke = data
+        //console.log(data)
+        const p = document.createElement('p')
+        p.innerText = data.value
+        document.body.append(p)
+
+    })
+
+setTimeout(function(){
+    console.log(joke)
+}, 1000)
+
